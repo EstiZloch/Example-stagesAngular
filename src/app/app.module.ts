6c9393eb23ed19de1391edbd22a139799f22c822
Esther <zloch.rachel@gmail.com>
2021-06-04 16:07:35 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormPageComponent } from './pages/form-page/form-page.component';
+import { StepsComponent } from './components/steps/steps.component';
+import { StepTemplateComponent } from './components/step-template/step-template.component';
+import { CompletePageComponent } from './pages/complete-page/complete-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormPageComponent,
+    StepsComponent,
+    StepTemplateComponent,
+    CompletePageComponent
   ],
   imports: [
     BrowserModule,
